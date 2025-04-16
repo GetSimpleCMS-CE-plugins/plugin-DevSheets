0991dce96663e84e96d3c67b6e3d7721052fe3bd
risingisland <risingisland@yahoo.com>
2025-04-16 16:39:58 +0100

    update

--- DevSheets/tabs/Tools.php
+++ DevSheets/tabs/Tools.php
@@ -5,26 +5,67 @@
 
 <div class="w3-row">
 
-	<div class="w3-container w3-quarter w3-panel xw3-border w3-center">
-		<a href="https://www.iloveimg.com/"  target="_blank">
-			<!--img src="<?php echo $SITEURL; ?>plugins/DevSheets/assets/iloveimg.svg" width="150px"-->
-			<svg xmlns="http://www.w3.org/2000/svg" style="width:150px" width="196.103" height="50.043" viewBox="1.949 0 196.103 50.043"><link xmlns="" type="text/css" id="dark-mode" rel="stylesheet" href=""/><style xmlns="" type="text/css" id="dark-mode-custom-style"/><path fill="#4D90FE" d="M64.604 1.522c-3.193 1.541-5.588 4.138-7.338 7.082C54.454 3.877 49.983.042 43.213.042c-6.774 0-15.078 5.749-15.078 15.459 0 9.832 8.204 14.492 12.267 17.123 4.343 2.81 11.754 7.92 16.864 17.375 5.112-9.456 12.522-14.565 16.866-17.375 3.052-1.979 8.446-5.103 10.936-10.709L64.604 1.522zm-.245 20.952V3.999l18.476 18.475H64.359z"/><path fill="#FFF" d="M64.359 22.474V3.999l18.476 18.475z"/><path d="M1.949 39.307H4.24c.727 0 1.565-1.006 1.565-1.677V13.318c0-.671-.838-1.621-1.565-1.621H1.949V5.828h18.388v5.869H17.99c-.727 0-1.509.95-1.509 1.621V37.63c0 .671.782 1.677 1.509 1.677h2.347v5.645H1.949v-5.645zm85.87.094h2.314c.734 0 1.58-1.016 1.58-1.693V13.155c0-.677-.847-1.637-1.58-1.637h-2.314V5.592h18.569v5.926h-2.371c-.734 0-1.524.96-1.524 1.637v24.552c0 .677.79 1.693 1.524 1.693h2.371v5.701H87.819v-5.7zm22.464 0h1.919c.79 0 1.637-1.016 1.637-1.693V13.155c0-.677-.846-1.637-1.637-1.637h-1.919V5.592h18.739l6.152 22.915h.169l6.209-22.915h18.682v5.926h-1.975c-.734 0-1.58.96-1.58 1.637v24.552c0 .677.846 1.693 1.58 1.693h1.975v5.701h-17.722V39.4h3.555V13.607h-.169l-8.749 31.495h-6.773l-8.636-31.495h-.169v25.794h3.725v5.701h-15.013v-5.701zm50.742-13.829c0-13.151 7.451-20.488 19.642-20.488 2.879 0 5.193.282 6.435.508h7.563v12.982h-6.998l-1.073-5.757c-1.073-.395-2.765-.734-4.797-.734-5.475 0-9.37 3.669-9.37 13.038 0 8.974 2.653 13.772 8.353 13.772 1.976 0 3.048-.169 4.177-.508v-6.942h-3.387v-5.757h16.481v5.813h-1.129c-.79 0-1.58 1.016-1.58 1.693v11.909h-5.475s-.508-1.185-1.581-1.185c-2.032 1.016-4.741 1.693-8.184 1.693-12.36 0-19.077-7.055-19.077-20.037z"/></svg>
-			<p>Compress & Resize Images</p>
-		</a>
+	<div class="w3-container w3-third w3-panel w3-padding w3-border w3-center">
+
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/themes/classic.min.css"/> <!-- 'classic' theme -->
+		<script src="https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/pickr.min.js"></script>
+		
+		<p><svg xmlns="http://www.w3.org/2000/svg" style="vertical-align:middle;" width="1.3em" height="1.3em" viewBox="0 0 32 32"><rect width="32" height="32" fill="none"/><g fill="none"><path fill="#fbb8ab" d="M21.993 5.854c-6.707-3.42-14.656-2.1-18.279 3.883S2.398 23.68 8.87 27.517c3.18 1.886 8.312 3.811 10.398 1.244c2.044-2.516-.972-4.11-.268-5.502c1.133-2.24 6.2.699 9.515-2.922c3.734-4.082.009-11.151-6.522-14.483M24 19c-1.745 0-3-1.273-3-3s1.255-3 3-3s3 1.273 3 3s-1.255 3-3 3" stroke-width="1" stroke="#fbb8ab"/><path fill="#8d65c5" d="M14.25 11a2.25 2.25 0 1 0 0-4.5a2.25 2.25 0 0 0 0 4.5" stroke-width="1" stroke="#8d65c5"/><path fill="#f70a8d" d="M8.25 15a2.25 2.25 0 1 0 0-4.5a2.25 2.25 0 0 0 0 4.5" stroke-width="1" stroke="#f70a8d"/><path fill="#00d26a" d="M8.25 22a2.25 2.25 0 1 0 0-4.5a2.25 2.25 0 0 0 0 4.5" stroke-width="1" stroke="#00d26a"/><path fill="#3f5fff" d="M13.75 26.5a2.25 2.25 0 1 0 0-4.5a2.25 2.25 0 0 0 0 4.5" stroke-width="1" stroke="#3f5fff"/></g></svg><b> Pickr</b></p>
+		
+		<div class="pickr-container"></div>
+		
+		<script>
+			// Simple example, see optional options for more configuration.
+			const pickr = Pickr.create({
+				el: '.pickr-container',
+				theme: 'classic', // or 'monolith', or 'nano'
+
+				swatches: [
+					'rgba(244, 67, 54, 1)',
+					'rgba(233, 30, 99, 0.95)',
+					'rgba(156, 39, 176, 0.9)',
+					'rgba(103, 58, 183, 0.85)',
+					'rgba(63, 81, 181, 0.8)',
+					'rgba(33, 150, 243, 0.75)',
+					'rgba(3, 169, 244, 0.7)',
+					'rgba(0, 188, 212, 0.7)',
+					'rgba(0, 150, 136, 0.75)',
+					'rgba(76, 175, 80, 0.8)',
+					'rgba(139, 195, 74, 0.85)',
+					'rgba(205, 220, 57, 0.9)',
+					'rgba(255, 235, 59, 0.95)',
+					'rgba(255, 193, 7, 1)'
+				],
+
+				components: {
+
+					// Main components
+					preview: true,
+					opacity: true,
+					hue: true,
+
+					// Input / output Options
+					interaction: {
+						hex: true,
+						rgba: true,
+						hsla: true,
+						hsva: true,
+						cmyk: true,
+						input: true,
+						clear: true,
+						save: true
+					}
+				}
+			});
+		</script>
 	</div>
 	
-	<div class="w3-container w3-quarter w3-panel xw3-border w3-center">
-		<a href="https://www.photopea.com/"  target="_blank">
-			<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><rect width="24" height="24" fill="none"/><path fill="#18A497" d="M20.098 0A3.9 3.9 0 0 1 24 3.903v16.194A3.9 3.9 0 0 1 20.098 24H6.393l-.051-10.34v-.074c0-3.92 3.112-7.09 6.963-7.09c2.31 0 4.177 1.902 4.177 4.254s-1.867 4.254-4.177 4.254c-.77 0-1.393-.634-1.393-1.418s.623-1.418 1.393-1.418c.769 0 1.392-.634 1.392-1.418s-.623-1.418-1.392-1.418c-2.31 0-4.178 1.9-4.178 4.253c0 2.352 1.868 4.254 4.178 4.254c3.85 0 6.962-3.169 6.962-7.09S17.155 3.66 13.305 3.66c-5.39 0-9.75 4.436-9.75 9.925v.086l.023 10.315A3.9 3.9 0 0 1 0 20.097V3.903A3.9 3.9 0 0 1 3.902 0z" stroke-width="0.5" stroke="#18A497"/></svg>
-			<p>Online Photo Editor</p>
-		</a>
+	<div class="w3-container w3-third w3-panel xw3-border w3-center">
+		
 	</div>
 	
-	<div class="w3-container w3-quarter w3-panel xw3-border w3-center">
-		<a href="https://meettheipsums.com/"  target="_blank">
-			<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><rect width="24" height="24" fill="none"/><path fill="#FFCC4D" d="M9 7h6a2 2 0 0 1 2 2v8h-2V9h-2v7h-2V9H9v8H7V9a2 2 0 0 1 2-2m3-5a10 10 0 0 1 10 10a10 10 0 0 1-10 10A10 10 0 0 1 2 12A10 10 0 0 1 12 2m0 2a8 8 0 0 0-8 8a8 8 0 0 0 8 8a8 8 0 0 0 8-8a8 8 0 0 0-8-8" stroke-width="0.5" stroke="#FFCC4D"/></svg>
-			<p>Meet the Ipsums</p>
-		</a>
+	<div class="w3-container w3-third w3-panel xw3-border w3-center">
+		
 	</div>
 	
 	<div class="w3-container w3-quarter w3-panel xw3-border w3-center">
